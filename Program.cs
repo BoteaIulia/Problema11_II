0ee8e5777aebfa23f0fdd2116791fdb239024b4f
dynamics <31436988+dynamics1@users.noreply.github.com>
2022-12-01 23:56:11 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public static class Globals
+{
+	internal static int Main()
+	{
+		int n;
+		int a;
+		int s = 0;
+		("%d", n) = Convert.ToInt32(value: Console.ReadLine());
+		for (int i = 0; i < n; i++)
+		{
+			("%d", a) = Convert.ToInt32(value: Console.ReadLine());
+			s += invers(a);
+		}
+		return s;
+	}
+}
+
